b8b9ae148c54e5aaa67e4c95a02387516bce136b
Ashraful610 <ashalam610@gmail.com>
2022-11-15 15:38:18 +0600

    banner 30% done

--- src/Component/Home/Banner/Banner.js
+++ src/Component/Home/Banner/Banner.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Banner.css'
 import video1 from '../../../Videos/video-1.mp4'
 import video2 from '../../../Videos/video-2.mp4'
@@ -6,16 +6,25 @@ import video3 from '../../../Videos/video-3.mp4'
 import video4 from '../../../Videos/video-4.mp4'
 
 const Banner = () => {
-    const [video , setVideo] = useState(video1)
+    // const [active , setActive] = useState(false)
+    const [index , setIndex] = useState(0)
     const videos = [video1 , video2 , video3 ,video4]
 
     return (
         <div className='main-banner'>
-            <video src={video} className='bg-video' autoPlay loop muted type='video/mp4' />
-            <div className='space-y-5 absolute right-12 top-1/2'>
+            <video src={video1} className={(videos.indexOf(video1)=== index) ? 'bg-video active': 'bg-video'} autoPlay loop muted type='video/mp4' />
+            <video src={video2} className={(videos.indexOf(video2)=== index) ? 'bg-video active': 'bg-video'} autoPlay loop muted type='video/mp4' />
+            <video src={video3} className={(videos.indexOf(video3)=== index) ? 'bg-video active': 'bg-video'} autoPlay loop muted type='video/mp4' />
+            <video src={video4} className={(videos.indexOf(video4)=== index) ? 'bg-video active': 'bg-video'} autoPlay loop muted type='video/mp4' />
+            <div className="content space-y-5">
+                <h1 className='text-5xl text-white font-semibold uppercase'>Welcome to my Website</h1>
+                <p className='text-lg text-white '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur cum est aut possimus necessitatibus consectetur amet repudiandae quod similique, voluptate libero, debitis iste eos pariatur labore odio corrupti. Facilis, voluptate?</p>
+                <button className='btn border-2 border-green-800  duration-400 hover:bg-green-800 bg-transparent px-5 p-1' onClick={()=> console.log('hello')}>Travel Now</button>
+            </div>
+            <div className='space-y-5 absolute z-[800] right-12 top-1/2'>
                 {
-                    videos.map((video , index) =>
-                     <div key={index} className='w-[15px] h-[15px] hover:scale-150 duration-200 rounded-full bg-sky-300' onClick={()=> setVideo(video)}></div> ) 
+                    videos.map((video, index) =>
+                     <div key={index} className='w-[10px] h-[10px] hover:scale-150 duration-200 rounded-full bg-white hover:bg-green-500 cursor-pointer' onClick={()=>setIndex( index)}></div> ) 
                 }
             </div>
         </div>
