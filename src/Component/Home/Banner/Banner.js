5285458c95492c06c1c051ac4d9d0006c35e8bcd
Ashraful610 <ashalam610@gmail.com>
2022-11-15 22:53:01 +0600

    add react icon

--- src/Component/Home/Banner/Banner.js
+++ src/Component/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Banner.css'
+import { FaQuoteLeft } from 'react-icons/fa';
 import video1 from '../../../Videos/video-1.mp4'
 import video2 from '../../../Videos/video-2.mp4'
 import video3 from '../../../Videos/video-3.mp4'
@@ -17,9 +18,13 @@ const Banner = () => {
             <video src={video3} className={(videos.indexOf(video3)=== index) ? 'bg-video active': 'bg-video'} autoPlay loop muted type='video/mp4' />
             <video src={video4} className={(videos.indexOf(video4)=== index) ? 'bg-video active': 'bg-video'} autoPlay loop muted type='video/mp4' />
             <div className="content space-y-5">
-                <h1 className='text-5xl text-white font-semibold uppercase'>Welcome to my Website</h1>
+                <h1 className='text-4xl text-white font-semibold uppercase title'>
+                <FaQuoteLeft/>   The world is a book and those who do not travel read only one page
+                </h1>
                 <p className='text-lg text-white '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur cum est aut possimus necessitatibus consectetur amet repudiandae quod similique, voluptate libero, debitis iste eos pariatur labore odio corrupti. Facilis, voluptate?</p>
-                <button className='btn border-2 border-green-800  duration-400 hover:bg-green-800 bg-transparent px-5 p-1' onClick={()=> console.log('hello')}>Travel Now</button>
+                <button className='btn bg-green-700 hover:bg-transparent hover:border-2 border-green-800 hover:border-green-800  font-bold' onClick={()=> console.log('hello')}>
+                   Travel Now
+                </button>
             </div>
             <div className='space-y-5 absolute z-[800] right-12 top-1/2'>
                 {
